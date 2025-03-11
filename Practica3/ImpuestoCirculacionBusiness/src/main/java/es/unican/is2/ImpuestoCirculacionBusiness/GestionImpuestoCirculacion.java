d81145203b59fe49c21273290672d4e16f00f802
Javier Gandara <gandaralazajavier@gmail.com>
2025-03-11 20:32:15 +0100

    Entrega final

--- Practica3/ImpuestoCirculacionBusiness/src/main/java/es/unican/is2/ImpuestoCirculacionBusiness/GestionImpuestoCirculacion.java
+++ Practica3/ImpuestoCirculacionBusiness/src/main/java/es/unican/is2/ImpuestoCirculacionBusiness/GestionImpuestoCirculacion.java
@@ -1,10 +1,11 @@
 package es.unican.is2.ImpuestoCirculacionBusiness;
 
-import java.util.List;
+import java.time.LocalDate;
 
 import es.unican.is2.ImpuestoCirculacionCommon.Contribuyente;
 import es.unican.is2.ImpuestoCirculacionCommon.DataAccessException;
 import es.unican.is2.ImpuestoCirculacionCommon.IInfoImpuestoCirculacion;
+import es.unican.is2.ImpuestoCirculacionCommon.TipoMotor;
 import es.unican.is2.ImpuestoCirculacionCommon.Vehiculo;
 import es.unican.is2.ImpuestoCirculacionDAOH2.ContribuyentesDAO;
 import es.unican.is2.ImpuestoCirculacionDAOH2.VehiculosDAO;
@@ -27,6 +28,32 @@ public class GestionImpuestoCirculacion implements IInfoImpuestoCirculacion {
 		return null;
 	}
 
-
-
+    public boolean altaContribuyente(String nombre, String apellidos, String dni) {
+    	throw new UnsupportedOperationException("Unimplemented method 'altaContribuyente'");
+    }
+    
+    public boolean bajaContribuyente(String dni) {
+    	throw new UnsupportedOperationException("Unimplemented method 'bajaContribuyente'");
+    }
+    
+    public boolean altaVehiculo(String matricula, LocalDate fechaMatriculacion, TipoMotor motor, double potencia, String dni) {
+    	throw new UnsupportedOperationException("Unimplemented method 'altaVehiculo'");
+    }
+    
+    public boolean bajaVehiculo(String matricula, String dni) {
+    	throw new UnsupportedOperationException("Unimplemented method 'bajaVehiculo'");
+    }
+    
+    public boolean cambiaTitular(String matricula, String dniActual, String dniNuevo) {
+    	throw new UnsupportedOperationException("Unimplemented method 'cambiaTitular'");
+    }
+    
+    public Contribuyente consultaContribuyente(String dni) {
+    	throw new UnsupportedOperationException("Unimplemented method 'consultaContribuyente'");
+    }
+    
+    public Vehiculo consultaVehiculo(String matricula) {
+    	throw new UnsupportedOperationException("Unimplemented method 'consultaVehiculo'");
+    }
+    
 }
